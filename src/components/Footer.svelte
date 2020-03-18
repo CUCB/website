af8cf29da988e9cbcc262ad35946b7ac586d3430
James Rhodes <jarhodes314@gmail.com>
2020-03-18 01:08:47 +0100

    Put footer at the bottom of the page

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,6 +2,10 @@
     p {
         text-align: center;
         color: rgba(0,0,0,0.5);
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
     }
 </style>
 
@@ -9,4 +13,4 @@
 
 
 
-<p>Hello, I'm a footer</p>
+<p>Hello, I'm a footer. Email <a href="mailto:james">James</a>, he loves email</p>
