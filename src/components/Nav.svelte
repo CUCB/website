10ad1f0be816bf40ae5f1217f9b7052f7b0d3c98
James Rhodes <jarhodes314@gmail.com>
2020-06-14 21:43:06 +0000

    #43 mobile styling and logo animation

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,13 +1,15 @@
 <style>
   nav {
-    font-weight: 400;
-    padding: 0 1em;
     display: flex;
   }
 
   [aria-current] {
     position: relative;
     display: inline-block;
+  }
+
+  [aria-current]:first-child::after {
+    padding-left: 0.5em;
   }
 
   [aria-current]::after {
@@ -20,38 +22,64 @@
     bottom: -1px;
   }
 
+  a:first-child {
+    padding-left: 0;
+  }
+
   a {
     text-decoration: none;
-    padding: 1em 0.5em;
+    padding: 0.5em;
     display: block;
     color: var(--accent);
   }
 
-  a:not([aria-current]):hover {
-    position: relative;
-    display: inline-block;
+  a.split {
+    margin-left: auto;
   }
 
-  a:not([aria-current]):hover::after {
-    position: absolute;
-    content: "";
-    width: calc(100% - 1em);
-    height: 2px;
-    display: block;
-    bottom: -1px;
+  @media only screen and (max-width: 600px) {
+    nav {
+      overflow: hidden;
+      transition: max-height 0.7s;
+      flex-direction: column;
+      position: relative;
+      top: 0;
+    }
+    nav.hidden {
+      max-height: 0;
+    }
+    nav.visible {
+      max-height: 400px;
+      animation: fadeIn ease-in-out 0.3s;
+    }
+    @keyframes fadeIn {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+    a.split {
+      margin-left: unset;
+    }
   }
 </style>
 
 <script>
   export let segment;
+  export let user;
+  export let visible;
+
+  $: navClass = visible ? "visible" : "hidden";
+
+  let transitions = 0;
+  const transitionDelay = () => 100 * transitions++;
 </script>
 
-<nav>
+<nav class="{navClass}" on:click>
   <a aria-current="{segment === undefined ? 'page' : undefined}" href=".">
     home
-  </a>
-  <a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">
-    about
   </a>
   <a aria-current="{segment === 'book' ? 'page' : undefined}" href="book">
     book us!
@@ -59,7 +87,27 @@
   <a aria-current="{segment === 'join' ? 'page' : undefined}" href="join">
     join us!
   </a>
+  <a
+    aria-current="{segment === 'committee' ? 'page' : undefined}"
+    href="committee"
+  >
+    committee
+  </a>
+
+  {#if user.userId}
+    <a
+      aria-current="{segment === 'members' ? 'page' : undefined}"
+      href="members"
+    >
+      members
+    </a>
+    <a href="auth/logout" class="split">log out</a>
+  {:else}
+    <a href="auth/login" class="split">log in</a>
+  {/if}
+
   <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 	     the blog data when we hover over the link or tap it on a touchscreen
 	<a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a>  -->
 </nav>
+<!-- </div> -->
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,28 +3,32 @@
     display: grid;
     min-height: 100vh;
     grid-template-rows: auto 1fr auto;
+    max-width: 60em;
+    width: 100%;
+    margin: auto;
     align-items: stretch;
+    padding: 0.5em 2em;
+    box-sizing: border-box;
   }
 
   main {
     position: relative;
-    max-width: 56em;
-    padding: 2em 2em 0 2em;
+    padding-top: 1em;
     width: 100%;
+    max-width: 56em;
     box-sizing: border-box;
     justify-self: center;
   }
 
-  :global(footer) {
-    max-width: 40em;
-    padding: 0 2em 1em 2em;
-    justify-self: center;
+  @media (max-width: 800px) {
+    .layout {
+      padding: 0.5em 1em;
+    }
   }
 
-  .customiser {
-    position: fixed;
-    bottom: 10px;
-    right: 10px;
+  :global(footer) {
+    max-width: 40em;
+    justify-self: center;
   }
 </style>
 
@@ -56,7 +60,7 @@
   import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
   import Customiser from "../components/Customiser.svelte";
-  import { goto } from "@sapper/app";
+  import { goto, stores } from "@sapper/app";
   import { makeClient, client } from "../graphql/client";
   import { onMount, setContext } from "svelte";
   import { readable } from "svelte/store";
@@ -67,11 +71,16 @@
   export let accent;
   export let logo;
 
-  onMount(() => {
-    client.set(makeClient(fetch, window.location.href.split("/", 1)[0]));
-  });
-
   let query = { color, font, accent, logo };
+  let { page, session } = stores();
+
+  onMount(() => {
+    const browserDomain = window.location.href
+      .split("/", 3)
+      .slice(0, 3)
+      .join("/");
+    client.set(makeClient(fetch, browserDomain));
+  });
 
   $: color = query.color;
   $: font = query.font;
@@ -93,20 +102,17 @@
   <link
     rel="stylesheet"
     type="text/css"
-    href="themes/accent/{accent}.css?logo={logo}"
+    href="themes/accent/{accent}.css?{logo ? `logo=${logo}` : ``}"
   />
   <link rel="stylesheet" type="text/css" href="global.css" />
 </svelte:head>
 
 <div class="layout">
-  <Header {segment} />
+  <Header {segment} user="{$session}" />
 
   <main>
     <slot />
   </main>
 
   <Footer />
-  <div class="customiser">
-    <Customiser bind:value="{query}" on:submit="{update}" />
-  </div>
 </div>
