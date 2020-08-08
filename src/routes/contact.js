ad729b7a848371d8d0bfbc24f875091c3b6bccf3
James Rhodes <jarhodes314@gmail.com>
2020-08-08 20:20:27 +0100

    Logging contact form

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -12,6 +12,7 @@ export async function post(req, res, next) {
     casual_name: "Secretary",
     email: "secretary@cucb.co.uk",
   };
+  console.log(secretary);
 
   const client = new SMTPClient({
     host: "localhost",
@@ -22,23 +23,28 @@ export async function post(req, res, next) {
     ? `Booking enquiry information:\n\tOccasion:\t${occasion}\n\tDates:\t\t${dates}\n\tTimes:\t\t${times}\n\tVenue:\t\t${venue}\n\n`
     : ``;
 
-  client.send({
-    logger: {
-      debug: console.log,
-      info: console.info,
-      warn: console.warn,
-      error: console.error,
+  client.send(
+    {
+      logger: {
+        debug: console.log,
+        info: console.info,
+        warn: console.warn,
+        error: console.error,
+      },
+      from: `CUCB Online Contact Form <webmaster@cucb.co.uk>`,
+      "reply-to": `${name.trim()} <${email.trim()}>`,
+      to: `CUCB Secretary <${secretary.email}>`,
+      subject: `${name} — Online Enquiry`,
+      text: `Hello ${
+        secretary.casual_name
+      }!\n\nYou have been sent the following enquiry by ${name} (${email}).\n\n${enquiryInformation}Message:\n\n\n-----------------------\n\n${escapeHtml(
+        message,
+      )}\n\n-----------------------\n\nMany thanks!\n`,
     },
-    from: `CUCB Online Contact Form <webmaster@cucb.co.uk>`,
-    "reply-to": `${name.trim()} <${email.trim()}>`,
-    to: `CUCB Secretary <${secretary.email}>`,
-    subject: `${name} — Online Enquiry`,
-    text: `Hello ${
-      secretary.casual_name
-    }!\n\nYou have been sent the following enquiry by ${name} (${email}).\n\n${enquiryInformation}Message:\n\n\n-----------------------\n\n${escapeHtml(
-      message,
-    )}\n\n-----------------------\n\nMany thanks!\n`,
-  });
+    (err, message) => {
+      console.log(err || message);
+    },
+  );
   res.statusCode = 204;
   res.end();
 }
