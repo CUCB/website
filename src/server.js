193044b0a1036d1119822cd555c82572f6953581
James Rhodes <jarhodes314@gmail.com>
2020-08-08 02:20:25 +0100

    Committee images hosting on dev server

--- src/server.js
+++ src/server.js
@@ -38,6 +38,7 @@ polka()
   .all(`${GRAPHQL_PATH}`, function(req, res) {
     apiProxy.web(req, res, { target: GRAPHQL_REMOTE });
   })
+  .use("/images/committee", sirv("static/images/committee", { dev }))
   .use("/static", sirv("static", { dev }))
   .use(
     compression({ threshold: 0 }),
