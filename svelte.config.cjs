0f6ddd2aab47533bc83d0961de50d1133ca44961
James Rhodes <jarhodes314@gmail.com>
2021-05-03 20:19:04 +0100

    Run svelte-check in docker

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -16,11 +16,7 @@ module.exports = {
   // for more information about preprocessors
   preprocess: [mdsvex(), preprocessors],
   kit: {
-    // By default, `npm run build` will create a standard Node app.
-    // You can create optimized builds for different platforms by
-    // specifying a different adapter
     adapter: node(),
-
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
   },
