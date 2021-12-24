909ea9d73af0f880711c49c8d4d64a969d73f2a7
James Rhodes <jarhodes314@gmail.com>
2021-12-24 21:35:10 +0000

    Hopefully fix SSL/TLS

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -47,7 +47,13 @@ export async function post(req, res, next) {
 
     const client = new SMTPClient({
       host: process.env["EMAIL_HOST"],
-      ssl: process.env["EMAIL_SSL"] === "true",
+      ssl: process.env["EMAIL_SSL"] !== "true" ? false : undefined,
+      tls:
+        process.env["EMAIL_SSL"] === "true"
+          ? {
+              ciphers: "SSLv3",
+            }
+          : undefined,
       port: parseInt(process.env["EMAIL_PORT"]),
       user: process.env["EMAIL_USERNAME"],
       password: process.env["EMAIL_PASSWORD"],
