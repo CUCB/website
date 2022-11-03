40215faffe3d224181f68a38bc233c16f093399f
James Rhodes <jarhodes314@gmail.com>
2022-11-03 16:40:23 +0000

    Fix migrations

--- hasura/migrations/default/1641142920862_set_fk_cucb_calendar_subscriptions_calendar_type/down.sql
+++ hasura/migrations/default/1641142920862_set_fk_cucb_calendar_subscriptions_calendar_type/down.sql
@@ -1,1 +1,3 @@
 alter table "cucb"."calendar_subscriptions" drop constraint "calendar_subscriptions_calendar_type_fkey";
+DELETE FROM "cucb"."calendar_subscriptions_types" WHERE "name" = 'allgigs';
+DELETE FROM "cucb"."calendar_subscriptions_types" WHERE "name" = 'mygigs';
--- hasura/migrations/default/1641142920862_set_fk_cucb_calendar_subscriptions_calendar_type/up.sql
+++ hasura/migrations/default/1641142920862_set_fk_cucb_calendar_subscriptions_calendar_type/up.sql
@@ -1,3 +1,5 @@
+INSERT INTO "cucb"."calendar_subscriptions_types"("name") VALUES (E'mygigs');
+INSERT INTO "cucb"."calendar_subscriptions_types"("name") VALUES (E'allgigs');
 alter table "cucb"."calendar_subscriptions"
   add constraint "calendar_subscriptions_calendar_type_fkey"
   foreign key ("calendar_type")
