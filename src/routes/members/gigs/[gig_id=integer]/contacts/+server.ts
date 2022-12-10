61480549270f18c9a658dc847ee93aa7e02a855d
James Rhodes <jarhodes314@gmail.com>
2022-12-10 18:27:04 +0000

    Delete contact -> mikroorm

--- src/routes/members/gigs/[gig_id=integer]/contacts/+server.ts
+++ src/routes/members/gigs/[gig_id=integer]/contacts/+server.ts
@@ -5,10 +5,10 @@ import { GigContact } from "$lib/entities/GigContact";
 import { error, json } from "@sveltejs/kit";
 import { Record, String } from "runtypes";
 
-const UPDATABLE_FIELDS: (keyof GigContact)[] = ["gig", "calling", "client", "contact", "notes"];
-const REQUIRED_FIELDS = Record({ gig: String, contact: String });
+const UPDATABLE_FIELDS: (keyof GigContact)[] = ["calling", "client", "contact", "notes"];
+const REQUIRED_FIELDS = Record({ contact: String });
 
-export const POST = async ({ request, locals }: RequestEvent): Promise<Response> => {
+export const POST = async ({ request, locals, params }: RequestEvent): Promise<Response> => {
   if (UPDATE_GIG_DETAILS.guard(locals.session)) {
     const em = orm.em.fork();
     const body = Object.fromEntries(
@@ -16,13 +16,29 @@ export const POST = async ({ request, locals }: RequestEvent): Promise<Response>
     );
 
     if (REQUIRED_FIELDS.guard(body)) {
-      let gigContact = await em.upsert(GigContact, body);
+      let gigContact = await em.upsert(GigContact, { ...body, gig: params.gig_id });
       gigContact = await em.findOneOrFail(
         GigContact,
         { gig: gigContact.gig, contact: gigContact.contact },
         { populate: ["contact"] },
       );
       return json(gigContact);
+    } else {
+      throw error(400, "Missing gig or contact id");
+    }
+  } else {
+    throw error(403, "You're not allowed to do that");
+  }
+};
+
+export const DELETE = async ({ request, locals, params }: RequestEvent): Promise<Response> => {
+  if (UPDATE_GIG_DETAILS.guard(locals.session)) {
+    const em = orm.em.fork();
+    const body = Object.fromEntries(Object.entries(await request.json()).filter(([key, _]) => key == "contact"));
+
+    if (REQUIRED_FIELDS.guard(body)) {
+      await em.nativeDelete(GigContact, { ...body, gig: params.gig_id });
+      return new Response("");
     } else {
       throw error(400, "Missing gig or contact id");
     }
