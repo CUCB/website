e31c0cd3967b30f75cf3a273d76c8ac4c1c25ed7
James Rhodes <jarhodes314@gmail.com>
2022-12-11 00:30:18 +0000

    Migrate restoreinstrument -> mikroorm

--- src/routes/members/user/[id=integer]/instruments/+server.ts
+++ src/routes/members/user/[id=integer]/instruments/+server.ts
@@ -4,6 +4,7 @@ import type { RequestEvent } from "./$types";
 import orm from "$lib/database";
 import { UserInstrument } from "$lib/entities/UsersInstrument";
 import { error, json } from "@sveltejs/kit";
+import { Literal, Record, String } from "runtypes";
 
 export const DELETE = async ({ request, params: { id }, locals: { session } }: RequestEvent): Promise<Response> => {
   assertLoggedIn(session);
@@ -27,6 +28,37 @@ export const DELETE = async ({ request, params: { id }, locals: { session } }: R
       // ...otherwise we can delete the instrument entirely
       await em.nativeDelete(UserInstrument, { id: userInstrumentId });
       return json(null);
+    }
+  } else {
+    throw error(403, "You're not allowed to do that");
+  }
+};
+
+const RESTORE_DELETED = Record({ deleted: Literal(false), userInstrumentId: String });
+
+export const POST = async ({ request, locals: { session }, params: { id } }: RequestEvent): Promise<Response> => {
+  assertLoggedIn(session);
+
+  if (UPDATE_INSTRUMENTS(id).guard(session)) {
+    let body = await request.json();
+    if (RESTORE_DELETED.guard(body)) {
+      const id = body.userInstrumentId;
+      body = Object.fromEntries(
+        Object.entries(body)
+          .filter(([key, _]) => Object.keys(RESTORE_DELETED.fields).includes(key))
+          .filter(([key, _]) => key != "userInstrumentId"),
+      );
+      const em = orm.em.fork();
+      const userInstrument = await em.findOne(UserInstrument, { id }, { populate: ["instrument"] });
+      if (userInstrument) {
+        userInstrument.deleted = false;
+        await em.persistAndFlush(userInstrument);
+        return json(userInstrument);
+      } else {
+        throw error(400, "Unrecognised user instrument");
+      }
+    } else {
+      throw error(400, "Unrecognized change");
     }
   } else {
     throw error(403, "You're not allowed to do that");
