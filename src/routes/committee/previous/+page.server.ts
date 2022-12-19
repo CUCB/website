756dc8bea1707c2abe91212ecad7d0dc8cb5c6b7
James Rhodes <jarhodes314@gmail.com>
2022-12-19 17:47:53 +0000

    Kill most remaining references to graphql

--- src/routes/committee/previous/+page.server.ts
+++ src/routes/committee/previous/+page.server.ts
@@ -1,4 +1,3 @@
-import { handleErrors } from "../../../graphql/client";
 import type { PageServerLoad } from "./$types";
 import { Committee } from "$lib/entities/Committee";
 import orm from "$lib/database";
@@ -30,23 +29,18 @@ interface CommitteeMember {
 
 export const load: PageServerLoad = async ({ url }) => {
   let aprilFools = url.searchParams.get("aprilfool") !== null;
-  try {
-    const committeeRepository = orm.em.fork().getRepository(Committee);
-    const mikroOrmRes = await committeeRepository.find(
-      { started: { $lte: "now()" } },
-      {
-        offset: 1,
-        // A limit is required for offset to work, it's also probably sensible to ave
-        limit: 100,
-        orderBy: { started: QueryOrder.DESC },
-        fields: ["members", "started"],
-        populate: ["members.lookup_name", "members.position"],
-      },
-    );
-    const committees: Committee_<Date>[] = mikroOrmRes.map((c) => wrap(c).toObject());
-    return { aprilFools, committees };
-  } catch (e) {
-    console.error(e);
-    return handleErrors(e);
-  }
+  const committeeRepository = orm.em.fork().getRepository(Committee);
+  const mikroOrmRes = await committeeRepository.find(
+    { started: { $lte: "now()" } },
+    {
+      offset: 1,
+      // A limit is required for offset to work, it's also probably sensible to ave
+      limit: 100,
+      orderBy: { started: QueryOrder.DESC },
+      fields: ["members", "started"],
+      populate: ["members.lookup_name", "members.position"],
+    },
+  );
+  const committees: Committee_<Date>[] = mikroOrmRes.map((c) => wrap(c).toObject());
+  return { aprilFools, committees };
 };
--- src/routes/members/+layout.ts
+++ src/routes/members/+layout.ts
@@ -1,10 +1,7 @@
 import { assertLoggedIn } from "../../client-auth";
-import { client, clientCurrentUser, GraphQLClient } from "../../graphql/client";
 import type { LayoutLoad } from "./$types";
 
 export const load: LayoutLoad = async ({ fetch, parent }) => {
   const { session } = await parent();
   assertLoggedIn(session);
-  client.set(new GraphQLClient(fetch));
-  clientCurrentUser.set(new GraphQLClient(fetch, { role: "current_user" }));
 };
