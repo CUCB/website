131eee6c6acab08b610100449eaedb006b695f23
James Rhodes <jarhodes314@gmail.com>
2022-12-22 01:05:24 +0000

    Kill graphql from build

--- src/lib/database/test.ts
+++ src/lib/database/test.ts
@@ -5,6 +5,9 @@ let orm: MikroORM;
 export const makeOrm = async (env) => {
   if (!orm) {
     orm = await MikroORM.init(makeConfig(env));
+    await orm.migrator.up();
+    const { NecessaryDataSeeder } = await import("../../seeders/NecessaryDataSeeder");
+    await orm.seeder.seed(NecessaryDataSeeder);
   }
   return orm.em.fork();
 };
