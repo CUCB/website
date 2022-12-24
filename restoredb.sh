132b8f2b85e4ff8b3dc5789a13f974ee430bfb6c
James Rhodes <jarhodes314@gmail.com>
2022-12-24 20:25:29 +0000

    Ignore foreign key constraints when restoring a data backup

--- restoredb.sh
+++ restoredb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose exec -T postgres pg_restore --data-only --schema=cucb -U postgres -d postgres /backups/data.sql
+docker-compose exec -T postgres pg_restore --disable-triggers --data-only --schema=cucb -U postgres -d postgres /backups/data.sql
