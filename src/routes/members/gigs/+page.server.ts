177faef81b50405e183f5d4e26c40ef11553c0a7
James Rhodes <jarhodes314@gmail.com>
2022-12-26 21:54:15 +0000

    Remove data.ts

--- src/routes/members/gigs/+page.server.ts
+++ src/routes/members/gigs/+page.server.ts
@@ -1,12 +1,18 @@
 import { error } from "@sveltejs/kit";
 import { DateTime, Settings } from "luxon";
 import type { SignupGig } from "../types";
-import orm from "$lib/database";
 import { NOT_MUSIC_ONLY } from "$lib/permissions";
-import { fetchAvailableInstruments, fetchSignupGigs, fetchUserNotes } from "../data";
 import type { PageServerLoad } from "./$types";
 import { assertLoggedIn } from "../../../client-auth";
-import { fetchMultiGigSignupSummary, fetchMultiGigSummary, inCurrentMonth, inFuture } from "./queries";
+import {
+  fetchAllInstrumentsForUser,
+  fetchMultiGigSignup,
+  fetchMultiGigSignupSummary,
+  fetchMultiGigSummary,
+  fetchUserNotes,
+  inCurrentMonth,
+  inFuture,
+} from "./queries";
 
 export const load: PageServerLoad = async ({ locals: { session }, parent }) => {
   Settings.defaultZoneName = "Europe/London";
@@ -15,15 +21,14 @@ export const load: PageServerLoad = async ({ locals: { session }, parent }) => {
   assertLoggedIn(session);
 
   if (NOT_MUSIC_ONLY.guard(session)) {
-    const em = (await orm()).em.fork();
     // Sort the gigs before rendering since the database can't sort by computed field
     const [upcomingGigs, gigsInCurrentMonth, signup_gigs, user_notes, signup_instruments, signupSummary] =
       await Promise.all([
         fetchMultiGigSummary(session, inFuture),
         fetchMultiGigSummary(session, inCurrentMonth),
-        fetchSignupGigs(em, session),
-        fetchUserNotes(em, session),
-        fetchAvailableInstruments(em, session),
+        fetchMultiGigSignup(session, {}),
+        fetchUserNotes(session),
+        fetchAllInstrumentsForUser(session),
         fetchMultiGigSignupSummary(session, { gig: { allow_signups: true } }),
       ]);
 
