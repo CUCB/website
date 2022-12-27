7f3b250f92b7c313f3abc9f4e042915411b3196a
James Rhodes <jarhodes314@gmail.com>
2022-12-27 16:35:06 +0000

    Add unit tests for permissions

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import GigSignup from "../../components/Gigs/Signup.svelte";
-  import { SELECT_LINEUPS } from "$lib/permissions";
+  import { SELECT_GIG_LINEUPS } from "$lib/permissions";
   import { makeTitle } from "../../view";
   import type { PageData } from "./$types";
   export let data: PageData;
@@ -14,7 +14,7 @@
 <h1>Members</h1>
 
 <h2>Gig signup</h2>
-{#if SELECT_LINEUPS.guard(session)}
+{#if SELECT_GIG_LINEUPS.guard(session)}
   <p>You're an important person. You can <a href="/members/gigs/signups">view the gig signup summary</a>.</p>
 {/if}
 {#each gigSignups as gig}
