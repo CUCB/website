36c68e0052a99ee027f9f7fa8fbbb1bba3e80001
James Rhodes <jarhodes314@gmail.com>
2022-12-28 16:28:07 +0000

    Ensure user is logged in before accessing calendar links

--- src/routes/members/gigs/calendar/links/+server.ts
+++ src/routes/members/gigs/calendar/links/+server.ts
@@ -1,9 +1,10 @@
 import { json } from "@sveltejs/kit";
 import { assertLoggedIn } from "../../../../../client-auth";
 import { mygigsCalendarUrl, allgigsCalendarUrl } from "../../../../data/cal.php/+server";
+import type { RequestEvent } from "./$types";
 
-export async function GET({ locals: { session } }) {
-  assertLoggedIn(session);
+export async function GET({ locals }: RequestEvent): Promise<Response> {
+  const session = assertLoggedIn(locals.session);
 
   return json({
     allgigs: allgigsCalendarUrl(session.userId),
--- src/routes/members/gigs/calendar/my/+server.ts
+++ src/routes/members/gigs/calendar/my/+server.ts
@@ -1,6 +1,10 @@
-import { redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
+import { LOGGED_IN } from "$lib/permissions";
 import { mygigsCalendarUrl } from "../../../../data/cal.php/+server";
+import type { RequestEvent } from "./$types";
+import { assertLoggedIn } from "../../../../../client-auth";
 
-export async function GET({ locals: { session } }) {
+export async function GET({ locals }: RequestEvent) {
+  const session = assertLoggedIn(locals.session);
   throw redirect(302, mygigsCalendarUrl(session.userId));
 }
