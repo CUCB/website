54f7e0b178b09138ca72ec4bac9e7468e0084525
James Rhodes <jarhodes314@gmail.com>
2022-12-29 10:44:17 +0000

    Add link to list042 page for people authorized to access it

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import GigSignup from "../../components/Gigs/Signup.svelte";
-  import { SELECT_GIG_LINEUPS } from "$lib/permissions";
+  import { SELECT_GIG_LINEUPS, UPDATE_LIST042 } from "$lib/permissions";
   import { makeTitle } from "../../view";
   import type { PageData } from "./$types";
   export let data: PageData;
@@ -16,6 +16,12 @@
 <h2>Gig signup</h2>
 {#if SELECT_GIG_LINEUPS.guard(session)}
   <p>You're an important person. You can <a href="/members/gigs/signups">view the gig signup summary</a>.</p>
+{/if}
+{#if UPDATE_LIST042.guard(session)}
+  <p>
+    You are also allowed to <a href="/members/list042">update the list of emails authorized to sign up to the website</a
+    >.
+  </p>
 {/if}
 {#each gigSignups as gig}
   <!-- TODO find some way of convincing this that session.userId definitely exists -->
