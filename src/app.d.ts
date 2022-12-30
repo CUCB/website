cd65a3082e56826f66bbdf0a814755f52b1e435c
James Rhodes <jarhodes314@gmail.com>
2022-12-30 13:39:39 +0000

    Add save and destroy to locals.session

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,7 +2,17 @@
 
 declare namespace App {
   interface Locals {
-    session: {} | { userId: string; role: string; firstName: string; lastName: string; theme: Record<string, string> };
+    session:
+      | {}
+      | {
+          userId: string;
+          role: string;
+          firstName: string;
+          lastName: string;
+          theme: Record<string, string>;
+          save(): Promise<[string, string, import("cookie").CookieSerializeOptions] | null>;
+          destroy(): Promise<[string, import("cookie").CookieSerializeOptions] | null>;
+        };
   }
 
   interface PageData {}
